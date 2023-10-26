c4488baa90c1a0e1d05973f4b648bb48e6aca12b
Biscuit <gtaw@booskit.dev>
2023-10-26 14:49:40 +0200

    lSpD iS sO sPeCiAL

--- templates/generators/form-arrest-report.php
+++ templates/generators/form-arrest-report.php
@@ -115,7 +115,7 @@
 					'attributes' => 'required',
 					'title' => 'Select Prison Assignment',
 					'list' => $pg->listChooser('prisonAssignmentList'),
-					'hint' => '<span class="d-block text-center"><strong>N/A</strong>: Only when arresting a suspect in a holding cell at a station (misdemeanors).</span>
+					'hint' => '<span class="d-block text-center"><strong>N/A</strong>: Only when arresting a suspect in a holding cell at a station (misdemeanors - <strong>This does not apply for LSPD; all arrests must be sent to prison.</strong>).</span>
 					<hr>
 					<strong><span style="color: #FF7800">Male</span></strong>: Male suspects above the age of 18.<br>
 					<strong><span style="color: #A600FF">Female</span></strong>: Female suspects above the age of 18.<br>
